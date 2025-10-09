67fb3eb52d879e1febee031a49fd8514afe6c3ba
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-10-09 04:32:20 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdentityMvcDemoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
