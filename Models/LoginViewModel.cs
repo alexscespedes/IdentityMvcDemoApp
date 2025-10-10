06314858cce43995d46d3b8bbd96af9ab47d5ab4
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-10-10 05:07:53 -0400

    Add Register & Login View Models

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityMvcDemoApp.Models;
+
+public class LoginViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
